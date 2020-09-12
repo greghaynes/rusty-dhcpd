96bb14b4c0be1fb14170ae050a85876201f87295
Greg Haynes <greg@greghaynes.net>
2020-09-12 03:20:22 +0000

    cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 mod server;
 
+use dhcp4r::server as dhcp4rserver;
 use std::net::{Ipv4Addr, UdpSocket};
-use dhcp4r::{server as dhcp4rserver};
 
 const SERVER_IP: Ipv4Addr = Ipv4Addr::new(10, 40, 4, 122);
 
@@ -9,8 +9,7 @@ fn main() {
     let socket = UdpSocket::bind("0.0.0.0:67").unwrap();
     socket.set_broadcast(true).unwrap();
 
-    let srv = server::Server {
-    };
+    let srv = server::Server {};
 
     dhcp4rserver::Server::serve(socket, SERVER_IP, srv);
 }
--- src/server.rs
+++ src/server.rs
@@ -1,29 +1,21 @@
-use dhcp4r::{server, options, packet};
+use dhcp4r::{options, packet, server};
 
-pub struct Server {
-}
+pub struct Server {}
 
 impl server::Handler for Server {
-    fn handle_request(&mut self,
-                      server: &server::Server,
-                      in_packet: packet::Packet) {
+    fn handle_request(&mut self, server: &server::Server, in_packet: packet::Packet) {
         match in_packet.message_type() {
             Ok(options::MessageType::Discover) => {
                 // Handle discover
             }
 
-            Ok(options::MessageType::Request) => {
-            }
+            Ok(options::MessageType::Request) => {}
 
-            Ok(options::MessageType::Release) |
-            Ok(options::MessageType::Decline) => {
-            }
+            Ok(options::MessageType::Release) | Ok(options::MessageType::Decline) => {}
 
             _ => {}
         }
     }
 }
 
-impl Server {
-
-}
+impl Server {}
