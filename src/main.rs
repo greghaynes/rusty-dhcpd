c63136bf72e675bebd3376b2e7229d7f87020a49
Greg Haynes <greg@greghaynes.net>
2021-07-26 22:40:22 +0000

    Add web and leases api

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,78 @@
 mod config;
 mod leases;
-mod server;
+mod dhcpd;
+mod web;
 
 #[macro_use]
 extern crate slog;
 extern crate slog_async;
 extern crate slog_term;
 
-use dhcp4r::server as dhcp4rserver;
 use slog::Drain;
-use std::net::{Ipv4Addr, SocketAddrV4, UdpSocket};
+use std::net::Ipv4Addr;
+use std::sync::Arc;
 use std::time::Duration;
+use tokio::sync::oneshot;
+use warp::Filter;
 
-const SERVER_IP: Ipv4Addr = Ipv4Addr::new(0, 0, 0, 0);
-const SERVER_PORT: u16 = 67;
+const SERVER_IP: Ipv4Addr = Ipv4Addr::new(10, 60, 0, 1);
+const SERVER_PORT: u16 = 9067;
 
-#[tokio::main]
-async fn main() {
-    let decorator = slog_term::TermDecorator::new().build();
-    let drain = slog_term::FullFormat::new(decorator).build().fuse();
-    let drain = slog_async::Async::new(drain).build().fuse();
-    let logger = slog::Logger::root(drain, o!());
-    let config = config::Config {
-        bind_address: SocketAddrV4::new(SERVER_IP, SERVER_PORT),
-        bind_interface: Some("dhcp-server-if".to_string()),
+fn init_config() -> config::Config {
+    return config::Config {
+        server_ip: SERVER_IP,
+        listen_port: SERVER_PORT,
+        bind_interface: None,
         lease_start: Ipv4Addr::new(10, 60, 0, 10),
         lease_count: 24,
-        lease_duration: Duration::from_secs(300),
+        lease_duration: Duration::from_secs(3),
         lease_subnet_mask: Ipv4Addr::new(255, 255, 255, 0),
         lease_routers: vec![Ipv4Addr::new(10, 60, 0, 1)],
         lease_domain_servers: vec![Ipv4Addr::new(8, 8, 8, 8)],
     };
+}
 
-    let mut srv = server::Server::create(&config, logger);
-    let shutdown_notify = std::sync::Arc::new(tokio::sync::Notify::new());
-    srv.serve(shutdown_notify).await;
+fn init_logging() -> slog::Logger {
+    let decorator = slog_term::TermDecorator::new().build();
+    let drain = slog_term::FullFormat::new(decorator).build().fuse();
+    let drain = slog_async::Async::new(drain).build().fuse();
+    return slog::Logger::root(drain, o!());
+}
+
+static INDEX_HTML:&str = r#"<!DOCTYPE html>
+<html lang="en">
+  <body>
+    <h1>HELLO</h1>
+  </body>
+</html>
+"#;
+
+#[tokio::main]
+async fn main() {
+    let logger = init_logging();
+    let config = init_config();
+
+    let dhcp_shutdown = Arc::new(tokio::sync::Notify::new());
+
+    let dhcp_srv = Arc::new(dhcpd::Server::create(&config, logger));
+
+    let index = warp::path::end().map(|| warp::reply::html(INDEX_HTML));
+    
+    let dhcp_srv_filter = warp::any().map(move || -> Arc<dyn dhcpd::AbstractServer> {dhcp_srv.clone()});
+    let leases = warp::path("leases")
+        .and(dhcp_srv_filter)
+        .and_then(web::leases_handler);
+    let routes = index.or(leases);
+
+    tokio::select! {
+        _ = dhcp_srv.serve(dhcp_shutdown) => {},
+        _ = warp::serve(routes).run(([0, 0, 0, 0], 3030)) => {}
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[tokio::test]
+    async fn leases_integration() {
+    }
 }
